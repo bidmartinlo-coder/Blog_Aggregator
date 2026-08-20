72067803ecf74422cf4aba176cfe19dd6761c53b
Cardiloo/Martin <bidmartinlo@gmail.com>
2026-08-20 11:08:38 +0200

    final product plan

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,5 +1,11 @@
 -- +goose Up
-CREATE TABLE user(id INTEGER PRIMARY KEY, created_at TIMESTAMP NOT NULL, updated_at TIMESTAMP NOT NULL, name TEXT NOT NULL);
+CREATE TABLE users (
+    id UUID PRIMARY KEY, 
+    created_at TIMESTAMP NOT NULL, 
+    updated_at TIMESTAMP NOT NULL, 
+    name TEXT NOT NULL UNIQUE
+);
 
 -- +goose Down
 DROP TABLE users;
+
